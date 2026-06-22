12f2e61659205fe6e86e5df2a4af363df12833ce
VedTeredesai7137 <vedteredesai@gmail.com>
2026-06-22 09:20:21 +0530

    day 2 warehouse init done

--- simulation/main.py
+++ simulation/main.py
@@ -1,3 +1,22 @@
-from rich import print
+print("Program Started")
+import sys
+from pathlib import Path
 
-print("[green]Warehouse Swarm System Initialized[/green]")
+PROJECT_ROOT = Path(__file__).parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
+
+from simulation.warehouse import Warehouse
+
+
+def main():
+    warehouse = Warehouse(
+        width=30,
+        height=20
+    )
+
+    warehouse.generate()
+    warehouse.render()
+
+
+if __name__ == "__main__":
+    main()
---
+++ simulation/warehouse.py
@@ -0,0 +1,77 @@
+# warehouse.py
+
+class Warehouse:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = []
+
+    def create_empty_grid(self):
+        self.grid = [
+            ["." for _ in range(self.width)]
+            for _ in range(self.height)
+        ]
+
+    def add_shelves(self):
+        for row in range(2, self.height - 2, 3):
+            for col in range(2, self.width - 2):
+                self.grid[row][col] = "S"
+
+    def add_charging_stations(self):
+        self.grid[0][0] = "C"
+        self.grid[0][1] = "C"
+        self.grid[0][self.width - 1] = "C"
+        self.grid[0][self.width - 2] = "C"
+
+    def add_robot_spawn_area(self):
+        spawn_row = self.height - 2
+        for col in range(1, 6):
+            self.grid[spawn_row][col] = "R"
+
+    def generate(self):
+        self.create_empty_grid()
+        self.add_shelves()
+        self.add_charging_stations()
+        self.add_robot_spawn_area()
+
+    def render(self):
+        for row in self.grid:
+            print(" ".join(row))
+
+    def is_walkable(self, x, y):
+
+        if x < 0:
+            return False
+
+        if y < 0:
+            return False
+
+        if x >= self.width:
+            return False
+
+        if y >= self.height:
+            return False
+
+        return self.grid[y][x] != "S"
+
+    def get_neighbors(self, x, y):
+
+        directions = [
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0)
+        ]
+
+        neighbors = []
+
+        for dx, dy in directions:
+
+            nx = x + dx
+            ny = y + dy
+
+            if self.is_walkable(nx, ny):
+                neighbors.append((nx, ny))
+
+        return neighbors
